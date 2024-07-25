dd378fbacbff8c34625276dcc9be84100242d38e
kvitee <kvitee41@gmail.com>
2024-07-25 22:33:55 +0800

    Add telescope plugin for faster navigation and search.

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      {
+        'nvim-lua/plenary.nvim',
+      },
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+      },
+    },
+    keys = {
+      { 'tf', '<cmd>Telescope find_files<cr>' },
+      { 'tg', '<cmd>Telescope live_grep<cr>' },
+    },
+    config = function()
+      local telescope = require('telescope')
+
+      -- Setup telescope --
+      telescope.setup({
+        extensions = {
+          fzf = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+          },
+        },
+      })
+
+      -- Load Telescope extensions --
+      telescope.load_extension('fzf')
+    end
+  },
+}
