1cc4305f1833de9cb9c872b5891fdf2c5866bae4
kvitee <kvitee41@gmail.com>
2024-07-25 17:08:02 +0800

    Create core module with basic key bindings.

--- lua/config/default.lua
+++ lua/config/default.lua
@@ -4,5 +4,8 @@ require('core.vim')
 -- Netrw file explorer settings --
 require('core.netrw')
 
+-- Key mappings --
+require('core.mappings')
+
 -- Lazy.nvim setup --
 require('core.lazy')
---
+++ lua/core/mappings.lua
@@ -0,0 +1,3 @@
+-- Go to next or previous tab by L or H respectively --
+vim.keymap.set('n', 'L', '<cmd>tabnext<cr>')
+vim.keymap.set('n', 'H', '<cmd>tabprev<cr>')
