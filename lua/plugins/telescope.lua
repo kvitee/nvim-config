d8968d6dd90bf8b79ecce66bd9f839e16052d6b9
kvitee <kvitee41@gmail.com>
2024-09-13 10:18:32 +0000

    Add nvim-web-devicons plugin.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,9 @@ return {
         'nvim-lua/plenary.nvim',
       },
       {
+        'nvim-tree/nvim-web-devicons',
+      },
+      {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'make',
       },
