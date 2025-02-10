9c45e07d2ecf429346d744ae077a795cf4fa753c
kvitee <kvitee41@gmail.com>
2025-02-10 17:09:16 +0800

    Refactor telescope plugin spec.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,36 +3,30 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = {
-      {
-        'nvim-lua/plenary.nvim',
-      },
-      {
-        'nvim-tree/nvim-web-devicons',
-      },
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
       {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'make',
       },
     },
     keys = {
-      { 'tf', '<cmd>Telescope find_files<cr>' },
-      { 'tg', '<cmd>Telescope live_grep<cr>' },
+      { '<leader>ff', '<cmd>Telescope find_files<cr>' },
+      { '<leader>fg', '<cmd>Telescope live_grep<cr>' },
     },
-    config = function()
+    opts = {
+      extensions = {
+        fzf = {
+          override_generic_sorter = false,
+          override_file_sorter = true,
+        },
+      },
+    },
+    config = function(_, opts)
       local telescope = require('telescope')
 
-      -- Setup telescope --
-      telescope.setup({
-        extensions = {
-          fzf = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-          },
-        },
-      })
-
-      -- Load Telescope extensions --
+      telescope.setup(opts)
       telescope.load_extension('fzf')
-    end
+    end,
   },
 }
