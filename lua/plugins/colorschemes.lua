1ccac7a787da16355a6aa59f54f2636e09966f40
kvitee <kvitee41@gmail.com>
2025-02-10 15:08:45 +0800

    Refactor colorschemes plugins spec.

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,28 +2,25 @@ return {
   {
     'ellisonleao/gruvbox.nvim',
     lazy = true,
-    config = function()
-      require('gruvbox').setup({
-        -- Disable italics for strings and comments --
-        italic = {
-          strings = false,
-          comments = false,
-        },
-      })
-    end
+    opts = {
+      italic = {
+        strings = false,
+      },
+    },
   },
   {
     'Shatur/neovim-ayu',
     lazy = true,
-    config = function()
+    main = 'ayu',
+    opts = function()
       local colors = require('ayu.colors')
       colors.generate()
 
-      require('ayu').setup({
+      return {
         overrides = {
           LineNr = { fg = colors.ui }
         },
-      })
+      }
     end,
   },
   {
