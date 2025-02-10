5f120fa34c58ecfe0f2e290db3cf91a2310c6836
kvitee <kvitee41@gmail.com>
2025-02-10 15:26:05 +0800

    Refactor leap plugin spec.

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -4,15 +4,9 @@ return {
     dependencies = {
       'tpope/vim-repeat',
     },
-    config = function()
-      -- Setup leap --
-      require('leap').setup({})
-
-      -- Custom key mappings --
-      vim.keymap.set('n',        'f', '<Plug>(leap)')
-      vim.keymap.set('n',        'F', '<Plug>(leap-from-window)')
-      vim.keymap.set({'x', 'o'}, 'f', '<Plug>(leap-forward)')
-      vim.keymap.set({'x', 'o'}, 'F', '<Plug>(leap-backward)')
-    end
+    keys = {
+      { '<leader>f', '<Plug>(leap-forward)' },
+      { '<leader>F', '<Plug>(leap-backward)' },
+    },
   },
 }
