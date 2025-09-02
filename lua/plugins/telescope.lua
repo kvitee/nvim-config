11a6bd30cf0daff41c52ee883770f6df47029ffa
kvitee <kvitee41@gmail.com>
2025-09-02 13:09:13 +0800

    Power up Telescope plugin.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,18 +10,58 @@ return {
         build = 'make',
       },
     },
-    keys = {
-      { '<leader>ff', '<cmd>Telescope find_files<cr>' },
-      { '<leader>fg', '<cmd>Telescope live_grep<cr>' },
-    },
-    opts = {
-      extensions = {
-        fzf = {
-          override_generic_sorter = false,
-          override_file_sorter = true,
+    keys = function()
+      local builtin = require('telescope.builtin')
+
+      return {
+        { '<leader>ff', builtin.find_files, { desc = 'Telescope | Find files' }},
+        { '<leader>fg', builtin.live_grep, { desc = 'Telescope | Live grep' }},
+        { '<leader>fb',
+          function()
+            builtin.buffers({ initial_mode = 'normal' })
+          end,
+          { desc = 'Telescope | Buffers' }
         },
-      },
-    },
+      }
+    end,
+    opts = function()
+      local actions = require('telescope.actions')
+
+      return {
+        defaults = {
+          mappings = {
+            n = {
+              ['<C-CR>'] = actions.select_tab,
+              ['q'] = actions.close,
+            },
+            i = {
+              ['<C-CR>'] = actions.select_tab,
+              ['<C-q>'] = actions.close,
+              ['<C-j>'] = actions.move_selection_next,
+              ['<C-k>'] = actions.move_selection_previous,
+            },
+          },
+        },
+        pickers = {
+          buffers = {
+            mappings = {
+              n = {
+                ['x'] = actions.delete_buffer,
+              },
+              i = {
+                ['<C-x>'] = actions.delete_buffer,
+              },
+            },
+          },
+        },
+        extensions = {
+          fzf = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+          },
+        },
+      }
+    end,
     config = function(_, opts)
       local telescope = require('telescope')
 
