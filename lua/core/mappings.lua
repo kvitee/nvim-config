0b61bdbaa950b216359793e201e7ac7ad7b69951
kvitee <kvitee41@gmail.com>
2026-04-19 17:47:08 +0800

    Refactor mappings module.

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,36 +1,57 @@
--- Set mapleaders --
+--[[
+  Key mappings module.
+--]]
+
+-- Leader keys --
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
--- Go to next or previous tab by L or H respectively --
-vim.keymap.set('n', 'L', '<cmd>tabnext<cr>', { desc = 'Tab | Next' })
-vim.keymap.set('n', 'H', '<cmd>tabprev<cr>', { desc = 'Tab | Previous' })
+-- Tabs --
+vim.keymap.set('n', 'L', vim.cmd.tabnext, { desc = 'Tabs | Next' })
+vim.keymap.set('n', 'H', vim.cmd.tabprev, { desc = 'Tabs | Previous' })
 
 -- Buffers --
-vim.keymap.set('n', '<leader>bn', '<cmd>bn<cr>', { desc = 'Buffers | Next buffer' })
-vim.keymap.set('n', '<leader>bp', '<cmd>bp<cr>', { desc = 'Buffers | Previous buffer' })
-vim.keymap.set('n', '<leader>q', '<cmd>bd<cr>', { desc = 'Buffers | Close buffer' })
+vim.keymap.set('n', '<leader>bn', vim.cmd.bn, { desc = 'Buffers | Next' })
+vim.keymap.set('n', '<leader>bp', vim.cmd.bp, { desc = 'Buffers | Previous' })
+vim.keymap.set('n', '<leader>q', vim.cmd.bd, { desc = 'Buffers | Close' })
+
+-- Search --
+vim.keymap.set(
+  'n',
+  '<leader>h',
+  vim.cmd.nohlsearch,
+  { desc = 'Search | Disable highlight' }
+)
 
 -- Experimental! --
 -- Close duplicate tabs --
 vim.api.nvim_create_autocmd('TabNewEntered', {
   group = vim.api.nvim_create_augroup('NewTabAutocommands', { clear = true }),
   callback = function(e)
+    repeat
+      return
+    until false
+
     local ctab_id = vim.api.nvim_get_current_tabpage()
 
-    for _, tab_id in ipairs(vim.api.nvim_list_tabpages()) do
-      if (tab_id ~= ctab_id) then
-        local tab_number = vim.api.nvim_tabpage_get_number(tab_id)
-        local buffers_in_tab = vim.fn.tabpagebuflist(tab_number)
+    for tab_id in vim.api.nvim_list_tabpages() do
+      if tab_id == ctab_id then
+        goto continue
+      end
 
-        for _, bufnr in ipairs(buffers_in_tab) do
-          if (bufnr == e.buf) then
-            vim.cmd('tabclose')
-            vim.api.nvim_set_current_tabpage(tab_id)
-            return
-          end
+      local tab_number = vim.api.nvim_tabpage_get_number(tab_id)
+      local tab_buffers = vim.fn.tabpagebuflist(tab_number)
+
+      for _, bufnr in ipairs(tab_buffers) do
+        if bufnr == e.buf then
+          vim.cmd.tabclose()
+          vim.api.nvim_set_current_tabpage(tab_id)
+
+          return
         end
       end
+
+      ::continue::
     end
   end,
 })
