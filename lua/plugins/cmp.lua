de390289d22e160cc70fc71fa3786118ebf12883
kvitee <kvitee41@gmail.com>
2026-04-19 17:19:12 +0800

    Change cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -21,15 +21,35 @@ return {
         documentation = cmp.config.window.bordered(),
       }
 
+      opts.view = {
+        docs = {
+          auto_open = false,
+        },
+      }
+
       opts.mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-Space>'] = cmp.mapping(function()
+          if cmp.visible_docs() then
+            cmp.close_docs()
+          elseif cmp.visible() then
+            cmp.open_docs()
+          else
+            cmp.complete()
+          end
+        end, { 'i', 's' }),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<cr>'] = cmp.mapping.confirm({ select = true }),
+        ['<cr>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm({ select = #cmp.get_entries() == 1 })
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
         ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
           elseif vim.snippet.active({ direction = 1 }) then
             vim.schedule(function()
               vim.snippet.jump(1)
@@ -40,7 +60,7 @@ return {
         end, { 'i', 's' }),
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_prev_item()
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
           elseif vim.snippet.active({ direction = -1 }) then
             vim.schedule(function()
               vim.snippet.jump(-1)
@@ -67,6 +87,13 @@ return {
         sources = cmp.config.sources({
           { name = 'path' },
           { name = 'cmdline' },
+        }),
+      })
+
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'buffer' },
         }),
       })
     end,
