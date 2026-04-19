11dc41142ac1c4a4dacb0d9678f3a8ab6afa0f4c
kvitee <kvitee41@gmail.com>
2026-04-19 17:57:12 +0800

    Add <leader> to all Neotree keybindings.

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,22 +7,23 @@ return {
       'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
     },
+    cmd = 'Neotree',
     keys = {
-      { 'nn', '<cmd>Neotree float<cr>' },
-      { 'nl', '<cmd>Neotree left<cr>' },
-      { 'ng', '<cmd>Neotree float git_status<cr>' },
-      { 'nc', '<cmd>Neotree close<cr>' },
+      { '<leader>nn', '<cmd>Neotree float<cr>' },
+      { '<leader>nl', '<cmd>Neotree left<cr>' },
+      { '<leader>ng', '<cmd>Neotree float git_status<cr>' },
+      { '<leader>nc', '<cmd>Neotree close<cr>' },
     },
     opts = {
       popup_border_style = 'rounded',
       default_component_configs = {
         git_status = {
           symbols = {
-            modified  = '',
-            renamed   = '',
-            ignored   = '',
-            unstaged  = '',
-            staged    = '',
+            modified = '',
+            renamed = '',
+            ignored = '',
+            unstaged = '',
+            staged = '',
           },
         },
       },
